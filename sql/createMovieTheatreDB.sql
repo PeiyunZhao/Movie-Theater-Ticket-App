f1389ca688890503d6de620f056f5b8bc71d898c
Karenzhang7717 <64809520+Karenzhang7717@users.noreply.github.com>
2020-11-25 21:47:29 -0700

    Added csv files

--- sql/createMovieTheatreDB.sql
+++ sql/createMovieTheatreDB.sql
@@ -4,11 +4,13 @@ USE movieTheatre;
 
 DROP TABLE IF EXISTS registered_user;
 CREATE TABLE registered_user (
-  uid			char(9) not null,
+  uid			varchar(20) not null,
   uname	        varchar(50) not null, 
+  upassword	        varchar(50) not null, 
+  uaddress	        varchar(50) not null, 
   uemail	        varchar(50) not null, 
-  uregdate	        varchar(50) not null, 
-  ucard  		char(10) not null,
+  upaiddate	        varchar(50) not null, 
+  ucard  		char(30) not null,
   primary key (uid)
 );
 
@@ -17,9 +19,33 @@ CREATE TABLE movie (
   mid			char(9) not null,
   mname         varchar(50) not null,
   mtype         varchar(30) not null,
-  mtime         varchar(60) not null,
-  mroom         varchar(20) not null,
+  mreserve      boolean not null,
   mprice	    float,
   primary key (mid)
   
 );
+
+DROP TABLE IF EXISTS showtime;
+CREATE TABLE showtime (
+  showid			char(9) not null,
+  showtime			varchar(20) not null,
+  showroom         varchar(20) not null,
+  primary key (showid)
+  
+);
+
+DROP TABLE IF EXISTS seat;
+CREATE TABLE seat (
+  seatid			char(9) not null,
+  userid           char(9),
+  ticketid        char(9),
+  sshowid        varchar(50),
+  seatcol         varchar(30) not null,
+  seatrow        varchar(60) not null,
+  seattaken      boolean not null,
+  
+  primary key (seatid)
+  
+);
+
+
